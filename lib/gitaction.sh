84a11e04c8988a0d85ed11ff86c03eaa4c0dd715
François Daoust <francois@joshfire.com>
2013-10-18 16:26:35 +0200

    First working version of deploy machine code
    The deploy machine is an HTTP server and a background daemon that
    runs git-related actions whenever:
    
    1. a post-receive hook is received on GitHub repos monitored by the
    server
    2. a periodic "check" script tells the daemon that the action should
    be run.
    
    In essence, for the Web site, the Web server reacts on "git push"
    commands on the "master" branch to build a new version of the Web
    site and on "git push" commands on the "build" branch to publish
    the version to the production server.
    
    Credentials should be set in environment variables. Typically, the
    code more or less expects the environment to define a:
    
     JOSHFIREDEPLOY_KEY_DEPLOYMACHINE
    
    ... variable that contains the private SSH key of the deploy machine.
    That key must have pull/push access to the repositories the deploy
    machine interacts with.
    
    Related to JOSHFIRE-22

---
+++ lib/gitaction.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+#######################################################
+# Script that retrieves the latest version of a repo
+# and runs the given script on the given branch when
+# done.
+#
+# The script takes 5 parameters:
+# - the origin URL of the repo
+# - the relative path to the "data" folder that will
+# contain the "repositories" folder
+# - the branch to switch to
+# - the relative path (from the root of the repo) to
+# the script to run
+# - the relative path (from the root of the repo) to
+# the script to run to check whether the first script
+# needs to run or not.
+#
+# The last parameter is optional. In its absence, the
+# first script will be run no matter what. If present,
+# the underlying script must exit with exit code 42 if
+# the first script needs to run, something else not to
+# run the first script.
+#
+# TODO: when "npm install" fails, the local repo folder
+# should be removed before the script returns an error,
+# because subsequent runs of the script will take for
+# granted that the repo is up and running otherwise.
+#######################################################
+
+REPO=$1
+DATAFOLDER=$2
+BRANCH=$3
+SCRIPT=$4
+CHECK=$5
+
+echo "Retrieve latest version of ${REPO} (branch ${BRANCH})..."
+
+if [ -z "${REPO}" ]
+then
+  echo ""
+  echo "No Git URL specified!"
+  echo ""
+  exit 1
+fi
+
+if [ -z "${BRANCH}" ]
+then
+  echo ""
+  echo "No branch specified!"
+  echo ""
+  exit 1
+fi
+
+if [ -z "${SCRIPT}" ]
+then
+  echo ""
+  echo "No script to run!"
+  echo ""
+  exit 1
+fi
+
+# Automatic error handling
+set -e
+
+echo "Create repositories folder if needed..."
+if [ ! -d "${DATAFOLDER}" ]
+then
+  mkdir ${DATAFOLDER}
+fi
+cd ${DATAFOLDER}
+
+if [ ! -d "repositories" ]
+then
+  mkdir repositories
+  echo "Create repositories folder if needed... done"
+else
+  echo "Create repositories folder if needed... not needed"
+fi
+
+cd repositories
+REPOFOLDER=${REPO//[:@\.\/]/-}
+if [ ! -d "${REPOFOLDER}" ]
+then
+  echo "Clone repository..."
+  git clone ${REPO} ${REPOFOLDER}
+  cd ${REPOFOLDER}
+  npm install
+  cd ..
+  echo "Clone repository... done"
+fi
+cd ${REPOFOLDER}
+
+echo "Switch to branch ${BRANCH}..."
+git fetch origin
+git checkout ${BRANCH}
+echo "Switch to branch ${BRANCH}... done"
+
+echo "Ensure we have latest version of ${BRANCH} branch..."
+remote=$(git log HEAD..origin/${BRANCH} --oneline)
+if [ "${remote}" != "" ]
+then
+  echo "Pull latest version of repository..."
+  git pull
+  npm install
+  echo "Pull latest version of repository... done"
+fi
+echo "Ensure we have latest version of ${BRANCH} branch... done"
+
+echo "Retrieve latest version of ${REPO} (branch ${BRANCH})... done"
+echo ""
+
+# Back to manual error handling
+set +e
+
+needsUpdate=42
+if [ ! -z "${CHECK}" ]
+then
+  echo "Run check script..."
+  ${CHECK}
+  needsUpdate=$?
+  echo "Run check script... done, response=${needsUpdate}"
+fi
+
+if [ "${needsUpdate}" == "42" ]
+then
+  echo "Run action script..."
+  ${SCRIPT}
+  result=$?
+  echo "Run action script... done, result=${result}"
+else
+  echo "No update needed"
+fi
+
+cd ../../..
+
+exit ${result}
---
+++ server.js
@@ -0,0 +1,132 @@
+/**
+ * @fileOverview Simple HTTP Web server that receives POST notifications
+ * from GitHub whenever a push is made and runs Git-related actions to
+ * react on these updates.
+ *
+ * The server also runs a monitoring task once every 20 minutes by default
+ * to react on external updates (e.g. external feeds that change over time).
+ */
+
+var githubhook = require('githubhook');
+var config = require('./lib/config');
+var TaskQueue = require('./lib/filequeue');
+var gitaction = require('./lib/gitaction');
+var woodman = require('woodman');
+var fs = require('fs');
+var path = require('path');
+
+woodman.load(config.WOODMAN || 'console');
+var logger = woodman.getLogger('server');
+logger.log('Server starting...');
+
+// Ensure the folder that will contain tasks, repos and deploy keys exists
+var dataFolder = config.DATA_FOLDER || 'data';
+logger.log('Create folder ' + dataFolder + '...');
+if (fs.existsSync(dataFolder)) {
+  logger.log('Create folder ' + dataFolder + '... not needed');
+}
+else {
+  fs.mkdirSync(dataFolder);
+  logger.log('Create folder ' + dataFolder + '... done');
+}
+
+// Create the task queue along with the worker function
+var taskqueue = new TaskQueue(gitaction, {
+  taskFolder: path.join(dataFolder, 'tasks'),
+  maxItems: 1
+});
+
+
+// Save deploy keys in "deploykeys" folder
+var deploykeysFolder = path.join(dataFolder, 'deploykeys');
+logger.log('Save deploy keys in ' + deploykeysFolder + '...');
+if (!fs.existsSync(deploykeysFolder)) {
+  fs.mkdirSync(deploykeysFolder);
+}
+var deploykeys = Object.keys(config).filter(function (key) {
+  return key.match(/^KEY_/);
+});
+deploykeys.forEach(function (name) {
+  logger.log('Save deploy key ' + name + '...');
+  fs.writeFileSync(
+    path.join(deploykeysFolder, name),
+    config[name], {
+      encoding: 'utf8',
+      mode: 384 // 600 in octal, restricted rights on private SSH key
+    }
+  );
+  logger.log('Save deploy key ' + name + '... done');
+});
+logger.log('Save deploy keys in ' + deploykeysFolder + '... done');
+
+
+// Prepare GitHub hooks listener
+var github = githubhook({
+  host: config.HOST || '0.0.0.0',
+  port: config.PORT || '3240',
+  secret: config.HOOKSECRET || '',
+  logger: woodman.getLogger('githubhook')
+});
+
+// Register all Post receive GitHub hooks
+logger.log('register GitHub hooks...');
+var hooks = config.POST_RECEIVE_HOOKS || {};
+Object.keys(hooks).forEach(function (name) {
+  var hook = hooks[name];
+  var eventName = (hook.event || 'push');
+  if (hook.reponame) {
+    eventName += ':' + hook.reponame;
+    if (hook.ref) {
+      eventName += ':' + hook.ref;
+    }
+  }
+  logger.log('register GitHub hook handler for event ' + eventName);
+  github.on(eventName, function () {
+    // Number of arguments depends on the event name, but last argument
+    // is always the whole data object returned by GitHub
+    var data = arguments[arguments.length - 1] || {};
+    data.repository = data.repository || {};
+    logger.info('queue action for ' + (hook.event || 'push') +
+      ' notification on repo ' + data.repository.name +
+      ' (ref ' + data.ref + ')');
+    var params = JSON.parse(JSON.stringify(hook.action));
+    params.from = 'github';
+    params.dataFolder = dataFolder;
+    params.privatekey = params.privatekey || 'KEY_DEPLOYMACHINE';
+    taskqueue.push(params);
+  });
+});
+logger.log('register GitHub hooks... done');
+
+
+// Start monitoring (run every 20 minutes by default)
+logger.log('start monitoring...');
+var interval = parseInt('' + (config.PERIODIC_INTERVAL || '1200'), 10);
+logger.log('monitoring interval is ' + interval + ' seconds');
+setInterval(function () {
+  logger.info('periodic check...');
+  if (taskqueue.getNbRunningTasks() > 0) {
+    logger.info('periodic check... postponed (running task detected)');
+    return;
+  }
+  var hooks = config.PERIODIC_HOOKS || {};
+  Object.keys(hooks).forEach(function (name) {
+    var hook = hooks[name];
+    var params = JSON.parse(JSON.stringify(hook.action));
+    params.from = 'monitoring';
+    params.dataFolder = dataFolder;
+    params.privatekey = params.privatekey || 'KEY_DEPLOYMACHINE';
+    logger.log('queued new monitoring action: ' + name);
+    taskqueue.push(params);
+  });
+  logger.info('periodic check... done');
+}, interval * 1000);
+logger.log('start monitoring... done, ' +
+  Object.keys(config.PERIODIC_HOOKS || {}).length + ' tasks monitored');
+
+
+// Start GitHub hooks listener
+github.listen(function () {
+  logger.info('Server started on port ' + (config.PORT || '3240'));
+  logger.log('Waiting for notifications...');
+});
