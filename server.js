4612ce8977dfcca51f2a1e6d7e79cec20b21645b
François Daoust <francois@joshfire.com>
2013-10-21 14:04:32 +0200

    Temporarily log SSH key to ensure the right one is used

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ var deploykeys = Object.keys(config).filter(function (key) {
 });
 deploykeys.forEach(function (name) {
   logger.log('Save deploy key ' + name + '...');
+  logger.log(config[name]);
   fs.writeFileSync(
     path.join(deploykeysFolder, name),
     config[name], {
