6c0c3af84bb38beb783f174b6f2e14dfa9f1ca16
François Daoust <francois@joshfire.com>
2013-10-24 12:19:07 +0200

    First version of separation between Web server and scheduler
    Beware, non working code! The goal is really just to park the code
    in case we want to get back to it later on.
    
    The approach will simply not work on Heroku: Web and scheduled dynos
    have separate filesystems so cannot communicate using a simple file
    queue. To implement things correctly, one would need to follow
    Heroku background jobs practices:
    https://devcenter.heroku.com/articles/background-jobs-queueing
    
    In particular, there would need to be:
    - one Web dyno
    - one worker dyno
    - a scheduler (the scheduler add-on should be enough)
    - some message queue (e.g. the CloudAMQP add-on to pass tasks)
    
    That seems overly complex for a simple deploy machine that will never
    have to scale, so sticking to an all-in-one approach for the time
    being.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * The server also runs a monitoring task once every 20 minutes by default
  * to react on external updates (e.g. external feeds that change over time).
  */
+/*global process*/
 
 var githubhook = require('githubhook');
 var config = require('./lib/config');
@@ -14,6 +15,7 @@ var gitaction = require('./lib/gitaction');
 var woodman = require('woodman');
 var fs = require('fs');
 var path = require('path');
+
 
 /**
  * Small helper function that deletes recursively deletes a folder
@@ -55,6 +57,10 @@ woodman.load(config.WOODMAN || 'console');
 var logger = woodman.getLogger('server');
 logger.log('Server starting...');
 
+// Check whether the idea is to run the Web server or the scheduled task
+var runScheduled = (process.argv[2] === 'scheduled');
+
+
 // Ensure the folder that will contain tasks, repos and deploy keys exists
 var dataFolder = config.DATA_FOLDER || 'data';
 logger.log('Create folder ' + dataFolder + '...');
@@ -66,117 +72,94 @@ else {
   logger.log('Create folder ' + dataFolder + '... done');
 }
 
-// Create the task queue along with the worker function
-var taskqueue = new TaskQueue(gitaction, {
-  taskFolder: path.join(dataFolder, 'tasks'),
-  maxItems: 1
-});
-
-logger.log('Clean deploy keys folder...');
 var deploykeysFolder = path.join(dataFolder, 'deploykeys');
-deleteFolder(deploykeysFolder);
-logger.log('Clean deploy keys folder... done');
-
-// The "repositories" folder may contain a hell of a lot of files.
-// To avoid spending one minute deleting files before the server
-// is up and running, let's park its contents to some other folder,
-// that will be suppressed after the server has started.
 var repositoriesFolder = path.join(dataFolder, 'repositories');
 var repositoriesBakFolder = path.join(dataFolder, 'repositories-bak');
-if (fs.existsSync(repositoriesFolder)) {
-  logger.log('Move repositories folder...');
-  if (fs.existsSync(repositoriesBakFolder)) {
-    deleteFolder(repositoriesBakFolder);
+var tasksFolder = path.join(dataFolder, 'tasks');
+
+if (!runScheduled) {
+  logger.log('Clean deploy keys folder...');
+  deleteFolder(deploykeysFolder);
+  logger.log('Clean deploy keys folder... done');
+
+  logger.log('Remove old lock if there is one...');
+  if (fs.existsSync(tasksFolder) &&
+      fs.existsSync(path.join(tasksFolder, 'lock'))) {
+    fs.unlinkSync(path.join(tasksFolder, 'lock'));
+    logger.log('Remove old lock if there is one... removed');
   }
-  fs.renameSync(repositoriesFolder, repositoriesBakFolder);
-  logger.log('Move repositories folder... done');
+  else {
+    logger.log('Remove old lock if there is one... not needed');
+  }
+
+  // The "repositories" folder may contain a hell of a lot of files.
+  // To avoid spending one minute deleting files before the server
+  // is up and running, let's park its contents to some other folder,
+  // that will be suppressed after the server has started.
+  if (fs.existsSync(repositoriesFolder)) {
+    logger.log('Move repositories folder...');
+    if (fs.existsSync(repositoriesBakFolder)) {
+      deleteFolder(repositoriesBakFolder);
+    }
+    fs.renameSync(repositoriesFolder, repositoriesBakFolder);
+    logger.log('Move repositories folder... done');
+  }
 }
 
 // Save deploy keys in "deploykeys" folder
-logger.log('Save deploy keys in ' + deploykeysFolder + '...');
-fs.mkdirSync(deploykeysFolder);
-var deploykeys = Object.keys(config).filter(function (key) {
-  return key.match(/^KEY_/);
-});
-deploykeys.forEach(function (name) {
-  logger.log('Save deploy key ' + name + '...');
-  fs.writeFileSync(
-    path.join(deploykeysFolder, name),
-    config[name], {
-      encoding: 'utf8',
-      mode: 384 // 600 in octal, restricted rights on private SSH key
-    }
-  );
-
-  // To use the private SSH key, we need to create a custom version of
-  // "ssh-noprompt.sh" that references the key. Note that the key cannot
-  // simply be set in the GIT_SSH_TMPKEY environment because that would
-  // only work for the "git clone" commands and not for the subsequent
-  // "npm install" call (which only preserves the GIT_SSH variable). In
-  // other words, this would not work if the repository depends on another
-  // private repository using a "git+ssh" URL.
-  var gitssh = 'ssh -i ' +
-    path.resolve(deploykeysFolder, name) +
-    ' -o IdentitiesOnly=yes' +
-    ' -o BatchMode=yes' +
-    ' -o UserKnownHostsFile=/dev/null' +
-    ' -o StrictHostKeyChecking=no' +
-    ' $@';
-  fs.writeFileSync(
-    path.join(deploykeysFolder, 'ssh-' + name + '.sh'),
-    gitssh, {
-      encoding: 'utf8',
-      mode: 448 // 700 in octal, to add execution rights
-    });
-  logger.log('Save deploy key ' + name + '... done');
-});
-logger.log('Save deploy keys in ' + deploykeysFolder + '... done');
-
-
-// Prepare GitHub hooks listener
-var github = githubhook({
-  host: config.HOST || '0.0.0.0',
-  port: config.PORT || '3240',
-  secret: config.HOOKSECRET || '',
-  logger: woodman.getLogger('githubhook')
-});
-
-// Register all Post receive GitHub hooks
-logger.log('register GitHub hooks...');
-var hooks = config.POST_RECEIVE_HOOKS || {};
-Object.keys(hooks).forEach(function (name) {
-  var hook = hooks[name];
-  var eventName = (hook.event || 'push');
-  if (hook.reponame) {
-    eventName += ':' + hook.reponame;
-    if (hook.ref) {
-      eventName += ':' + hook.ref;
-    }
-  }
-  logger.log('register GitHub hook handler for event ' + eventName);
-  github.on(eventName, function () {
-    // Number of arguments depends on the event name, but last argument
-    // is always the whole data object returned by GitHub
-    var data = arguments[arguments.length - 1] || {};
-    data.repository = data.repository || {};
-    logger.info('queue action for ' + (hook.event || 'push') +
-      ' notification on repo ' + data.repository.name +
-      ' (ref ' + data.ref + ')');
-    var params = JSON.parse(JSON.stringify(hook.action));
-    params.from = 'github';
-    params.dataFolder = dataFolder;
-    params.privatekey = params.privatekey || 'KEY_MAIN';
-    taskqueue.push(params);
+if (!fs.existsSync(deploykeysFolder)) {
+  logger.log('Save deploy keys in ' + deploykeysFolder + '...');
+  fs.mkdirSync(deploykeysFolder);
+  var deploykeys = Object.keys(config).filter(function (key) {
+    return key.match(/^KEY_/);
   });
+  deploykeys.forEach(function (name) {
+    logger.log('Save deploy key ' + name + '...');
+    fs.writeFileSync(
+      path.join(deploykeysFolder, name),
+      config[name], {
+        encoding: 'utf8',
+        mode: 384 // 600 in octal, restricted rights on private SSH key
+      }
+    );
+
+    // To use the private SSH key, we need to create a custom version of
+    // "ssh-noprompt.sh" that references the key. Note that the key cannot
+    // simply be set in the GIT_SSH_TMPKEY environment because that would
+    // only work for the "git clone" commands and not for the subsequent
+    // "npm install" call (which only preserves the GIT_SSH variable). In
+    // other words, this would not work if the repository depends on another
+    // private repository using a "git+ssh" URL.
+    var gitssh = 'ssh -i ' +
+      path.resolve(deploykeysFolder, name) +
+      ' -o IdentitiesOnly=yes' +
+      ' -o BatchMode=yes' +
+      ' -o UserKnownHostsFile=/dev/null' +
+      ' -o StrictHostKeyChecking=no' +
+      ' $@';
+    fs.writeFileSync(
+      path.join(deploykeysFolder, 'ssh-' + name + '.sh'),
+      gitssh, {
+        encoding: 'utf8',
+        mode: 448 // 700 in octal, to add execution rights
+      });
+    logger.log('Save deploy key ' + name + '... done');
+  });
+  logger.log('Save deploy keys in ' + deploykeysFolder + '... done');
+}
+
+
+// Create the task queue along with the worker function
+var taskqueue = new TaskQueue(gitaction, {
+  taskFolder: tasksFolder,
+  maxItems: 1
 });
-logger.log('register GitHub hooks... done');
 
 
-// Start monitoring (run every 20 minutes by default)
-logger.log('start monitoring...');
-var interval = parseInt('' + (config.PERIODIC_INTERVAL || '1200'), 10);
-logger.log('monitoring interval is ' + interval + ' seconds');
-setInterval(function () {
+/**
+ * Internal function that runs periodic checks
+ */
+var runPeriodicCheck = function () {
   logger.info('periodic check...');
   if (taskqueue.getNbRunningTasks() > 0) {
     logger.info('periodic check... postponed (running task detected)');
@@ -193,23 +176,83 @@ setInterval(function () {
     taskqueue.push(params);
   });
   logger.info('periodic check... done');
-}, interval * 1000);
-logger.log('start monitoring... done, ' +
-  Object.keys(config.PERIODIC_HOOKS || {}).length + ' tasks monitored');
+};
 
 
-// Start GitHub hooks listener and delete the "repositories" folder
-// (deletion is done afterwards because that may take time and some
-// server environments might impose a startup timeout. TODO: switch
-// to an async version, otherwise deletion could make incoming requests
-// time out)
-github.listen(function () {
-  logger.info('Server started on port ' + (config.PORT || '3240'));
-  logger.log('Waiting for notifications...');
+if (runScheduled) {
+  /**************************************************
+  Run one periodic check
+  **************************************************/
+  runPeriodicCheck();
+}
+else {
+  /**************************************************
+  Run the Web server that listens to post-receive
+  hooks from GitHub
+  **************************************************/
+  var github = githubhook({
+    host: config.HOST || '0.0.0.0',
+    port: config.PORT || '3240',
+    secret: config.HOOKSECRET || '',
+    logger: woodman.getLogger('githubhook')
+  });
 
-  if (fs.existsSync(repositoriesBakFolder)) {
-    logger.log('Delete former "repositories" folder...');
-    deleteFolder(repositoriesBakFolder);
-    logger.log('Delete repositories folder... done');
+  // Register all Post receive GitHub hooks
+  logger.log('register GitHub hooks...');
+  var hooks = config.POST_RECEIVE_HOOKS || {};
+  Object.keys(hooks).forEach(function (name) {
+    var hook = hooks[name];
+    var eventName = (hook.event || 'push');
+    if (hook.reponame) {
+      eventName += ':' + hook.reponame;
+      if (hook.ref) {
+        eventName += ':' + hook.ref;
+      }
+    }
+    logger.log('register GitHub hook handler for event ' + eventName);
+    github.on(eventName, function () {
+      // Number of arguments depends on the event name, but last argument
+      // is always the whole data object returned by GitHub
+      var data = arguments[arguments.length - 1] || {};
+      data.repository = data.repository || {};
+      logger.info('queue action for ' + (hook.event || 'push') +
+        ' notification on repo ' + data.repository.name +
+        ' (ref ' + data.ref + ')');
+      var params = JSON.parse(JSON.stringify(hook.action));
+      params.from = 'github';
+      params.dataFolder = dataFolder;
+      params.privatekey = params.privatekey || 'KEY_MAIN';
+      taskqueue.push(params);
+    });
+  });
+  logger.log('register GitHub hooks... done');
+
+
+  // Start monitoring if so requested
+  // (run every 20 minutes by default)
+  if (!config.PERIODIC_SCHEDULED) {
+    logger.log('start monitoring...');
+    var interval = parseInt('' + (config.PERIODIC_INTERVAL || '1200'), 10);
+    logger.log('monitoring interval is ' + interval + ' seconds');
+    setInterval(runPeriodicCheck, interval * 1000);
+    logger.log('start monitoring... done, ' +
+      Object.keys(config.PERIODIC_HOOKS || {}).length + ' tasks monitored');
   }
-});
+
+  // Start GitHub hooks listener and delete the "repositories" folder
+  // (deletion is done afterwards because that may take time and some
+  // server environments might impose a startup timeout. TODO: switch
+  // to an async version, otherwise deletion could make incoming requests
+  // time out)
+  github.listen(function () {
+    logger.info('Server started on port ' + (config.PORT || '3240'));
+    logger.log('Waiting for notifications...');
+
+    if (fs.existsSync(repositoriesBakFolder)) {
+      logger.log('Delete former "repositories" folder...');
+      deleteFolder(repositoriesBakFolder);
+      logger.log('Delete repositories folder... done');
+    }
+  });
+}
+ 
